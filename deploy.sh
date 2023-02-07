811d2270ee37edd49e454660400fbe0c598036b1
singhxbaljot <b.singh90@wlv.ac.uk>
2023-02-07 14:30:52 +0000

    w

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 read -p "Commit description: " desc
 git add -A .
 git commit -m "$desc" && \
-# git push https://github.com/singhxbaljot/awd-ci.git main
-
-git push https://singhxbaljot@github.com/singhxbaljot/awd-ci.git main
+git push https://github.com/singhxbaljot/awd-ci.git main
