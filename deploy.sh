b8aeac89ba66a06b27f323862a0c93a927d24aff
singhxbaljot <b.singh90@wlv.ac.uk>
2023-02-07 14:47:20 +0000

    exit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo TOKEN: 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
 read -p "Commit description: " desc
 git add -A .
 git commit -m "$desc" && \
 # git push https://github.com/singhxbaljot/awd-ci.git main
 
-git push 'https://singhxbaljot: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@github.com/singhxbaljot/awd-ci.git' main
+git push 'https://singhxbaljot@github.com/singhxbaljot/awd-ci.git' main
