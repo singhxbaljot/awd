2b04d09ab9702b05634f5c63037ac874a7771571
singhxbaljot <b.singh90@wlv.ac.uk>
2023-02-07 14:25:31 +0000

    with username and password

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git add -A .
 git commit -m "$desc" && \
 # git push https://github.com/singhxbaljot/awd-ci.git main
 
-git push https://singhxbaljot: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@awd-ci/file.git main
+git push https://singhxbaljot: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@github.com/singhxbaljot/awd-ci.git main
